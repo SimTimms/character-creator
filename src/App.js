8426bd9f15713b74b5fceb236dc4366c160ec88e
Tim Simms <36262574+SimTimms@users.noreply.github.com>
2018-08-07 20:34:38 +0100

    Build

--- src/App.js
+++ src/App.js
@@ -30,7 +30,6 @@ import {
   treasures,
 } from './data/index';
 const apiTarget = 'https://char-creator-api.herokuapp.com/upload-char';
-//const imageReturnURL = 'https://char-creator.herokuapp.com/my-hero-master';
 //const apiTarget = 'http://localhost:3001/upload-char';
 
 class App extends Component {
